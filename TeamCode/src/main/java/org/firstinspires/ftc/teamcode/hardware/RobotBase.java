c7b275c22c81a635c685978875dbdbde8f55d6b3
OwenRex <owenrexshell@4hscream.org>
2023-10-10 20:59:12 -0500

    Started Robot base class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotBase.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+
+
+public class RobotBase {
+
+
+
+    private DistanceSensor frontDistanceSensor;
+    private DistanceSensor leftDistanceSensor;
+    private DistanceSensor rightDistanceSensor;
+    private DistanceSensor backDistanceSensor;
+
+}
