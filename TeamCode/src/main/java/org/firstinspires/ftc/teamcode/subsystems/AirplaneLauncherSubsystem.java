5891a0aa3f7553632c935a4ba695e00e4ec92ec9
OwenBaller <owenball@4hscream.org>
2023-10-14 16:47:28 -0500

    AirplaneLauncherSubSystem hangingMechanismSuSystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AirplaneLauncherSubsystem.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class AirplaneLauncherSubsystem extends SubsystemBase {
+
+    private Servo srvAirplaneLauncher;
+
+    public AirplaneLauncherSubsystem (Servo airplaneLauncherConstructor) {
+        srvAirplaneLauncher = airplaneLauncherConstructor;
+    }
+        public void Raise () {
+            srvAirplaneLauncher.setPosition(1);
+        }
+    }
