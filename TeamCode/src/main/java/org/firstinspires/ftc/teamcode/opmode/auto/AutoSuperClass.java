84aa6cbe7b5905b7ef2f231946b0dbdc66533182
OwenRex <owenrexshell@4hscream.org>
2023-10-28 16:13:13 -0500

    Started supper class file for auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoSuperClass.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
+public class AutoSuperClass extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+@Override
+    public void init_loop(){}
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
