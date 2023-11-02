2da6ec2948ed6962107c3a579ed3c011e7ca4e5a
OwenBaller <owenball@4hscream.org>
2023-11-02 18:55:17 -0500

    t

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoSuperClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoSuperClass.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 public class AutoSuperClass extends OpMode {
-
     @Override
     public void init() {
 
