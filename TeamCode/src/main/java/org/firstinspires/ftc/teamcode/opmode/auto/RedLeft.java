589f7444f73fafa263bd6be45a5a074999d89582
PWikoff <prestonwikoff@4hscream.org>
2023-11-25 14:20:01 -0600

    Made autonomous route for V1 Robot for Red Left, Middle spike mark, Parking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/RedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/RedLeft.java
@@ -16,7 +16,7 @@ import org.firstinspires.ftc.teamcode.subsystems.DataStorageSubsystem;
 @Autonomous(name = "RedLeft")
 public class RedLeft extends OpMode {
     public RobotBase robotBase;
-    private TrajectorySequence RedLeftPark;
+    private TrajectorySequence RedLeftMiddleOuter;
     public Pose2d startPose;
 
     @Override
@@ -25,12 +25,15 @@ public class RedLeft extends OpMode {
         robotBase.alliance = RobotBase.Alliance.RED;
         robotBase.startPosition = RobotBase.StartPosition.LEFT;
         startPose = new Pose2d(-38.35, -63.3, Math.toRadians(90.00));
-        RedLeftPark  = robotBase.mecanumDrive.trajectorySequenceBuilder(new Pose2d(-38.35, -63.30, Math.toRadians(90.00)))
-                .lineToLinearHeading(new Pose2d(-38.35, -12.00, Math.toRadians(0.00)))
-                .waitSeconds(20)
-                .lineTo(new Vector2d(6.00, -12.00))
-                .lineTo(new Vector2d(50, -12.00))
+        RedLeftMiddleOuter  = robotBase.mecanumDrive.trajectorySequenceBuilder(new Pose2d(-39.00, -63.30, Math.toRadians(90.00)))
+                .splineToSplineHeading(new Pose2d(-35.17, -32.28, Math.toRadians(95.00)), Math.toRadians(95.00))
+                .splineToSplineHeading(new Pose2d(-33.00, -37.00, Math.toRadians(180.00)), Math.toRadians(0.00))
+                .lineToSplineHeading(new Pose2d(37.00, -37.00, Math.toRadians(180.00)))
+                .lineToSplineHeading(new Pose2d(51.00, -37.00, Math.toRadians(180.00)))
+                .lineToSplineHeading(new Pose2d(41.00, -37.00, Math.toRadians(180.00)))
+                .splineToConstantHeading(new Vector2d(61.00, -12.00), Math.toRadians(0.00))
                 .build();
+
 
         robotBase.mecanumDrive.setPoseEstimate(startPose);
     }
@@ -40,7 +43,7 @@ public class RedLeft extends OpMode {
     }
     @Override
     public void start(){
-        robotBase.mecanumDrive.followTrajectorySequence(RedLeftPark);
+        robotBase.mecanumDrive.followTrajectorySequence(RedLeftMiddleOuter);
     }
     @Override
     public void loop(){
