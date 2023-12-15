adda802ecc57aeb06419dea933bd6939cafccac9
FeignedBlock485 <benelven@4hscream.org>
2023-12-14 20:12:13 -0600

    Made first dual slide controlling arm bind. No further progress made. Wait for Jeremy to check so we don't have problems if this is wrong.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleDriverRobotControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleDriverRobotControl.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.gamepad.TriggerReader;
@@ -12,6 +13,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.commands.DropOffPositionCommand;
+import org.firstinspires.ftc.teamcode.commands.UniversalGrabbingPosCommand;
 import org.firstinspires.ftc.teamcode.hardware.RobotBase;
 import org.firstinspires.ftc.teamcode.subsystems.DataStorageSubsystem;
 
@@ -97,7 +100,10 @@ public class TeleDriverRobotControl extends OpMode {
 
 
         //DUAL SLIDE LOW
-
+        armController.getGamepadButton(GamepadKeys.Button.A)
+                .whenPressed(new SequentialCommandGroup(
+                        new DropOffPositionCommand(robotBase.leftSlideSubsystem, robotBase.armSubsystem, robotBase.leftWristSubsystem, robotBase.leftClawSubsystem, robotBase.rightClawSubsystem, robotBase.leftSlideSubsystem.slideHeight),
+                new DropOffPositionCommand(robotBase.rightSlideSubsystem, robotBase.armSubsystem, robotBase.rightWristSubsystem, robotBase.leftClawSubsystem, robotBase.rightClawSubsystem, robotBase.rightSlideSubsystem.slideHeight)));
 
         //LEFT SLIDE MEDIUM
 
@@ -138,6 +144,7 @@ public class TeleDriverRobotControl extends OpMode {
         Orientation angles = robotBase.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS);
         dblCurrentHeading = angles.firstAngle + DataStorageSubsystem.dblIMUFinalHeading;
 
+        new UniversalGrabbingPosCommand();
 
         if (robotBase.controlScheme == RobotBase.ChassisControlType.FIELDCENTRIC) {
             Vector2d input = new Vector2d(
@@ -179,53 +186,6 @@ public class TeleDriverRobotControl extends OpMode {
         if (chassisController.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.1 || chassisController.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.1) {
             robotBase.intakeSubsystem.intake(chassisController.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) -
                     chassisController.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER));
-        }
-
-        //ARM CONTROLLER BINDS
-
-        if (syncSlidesMode == RobotBase.SyncSlidesMode.OFF) {
-            //OPERATING THE RIGHT SLIDE AND CLAW
-            if (armController.wasJustPressed(GamepadKeys.Button.A)) {
-                robotBase.rightSlideSubsystem.slideLowToggle();
-            }
-            if (armController.wasJustPressed(GamepadKeys.Button.B)) {
-                robotBase.rightSlideSubsystem.slideMediumToggle();
-            }
-            if (armController.wasJustPressed(GamepadKeys.Button.Y) && !armController.getButton(GamepadKeys.Button.START)) {
-                robotBase.rightSlideSubsystem.slideHighToggle();
-            }
-            if (armController.wasJustPressed(GamepadKeys.Button.X)) {
-                robotBase.rightSlideSubsystem.slideTuningDown();
-            }
-           if (rightTriggerArmReader.wasJustPressed()) {
-                robotBase.rightSlideSubsystem.slideTuningUp();
-            }
-            if (armController.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)) {
-                robotBase.rightClawSubsystem.clawOpen();
-            }
-            //OPERATING THE LEFT SLIDE AND CLAW
-            if (armController.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)) {
-                robotBase.leftSlideSubsystem.slideLowToggle();
-            }
-            if (armController.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)) {
-                robotBase.leftSlideSubsystem.slideMediumToggle();
-            }
-            if (armController.wasJustPressed(GamepadKeys.Button.DPAD_UP)) {
-                robotBase.leftSlideSubsystem.slideHighToggle();
-            }
-            if (armController.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)) {
-                robotBase.leftSlideSubsystem.slideTuningDown();
-            }
-             if (leftTriggerArmReader.wasJustPressed()) {
-                 robotBase.leftSlideSubsystem.slideTuningUp();
-             }
-            if (armController.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
-                robotBase.leftClawSubsystem.clawOpen();
-            }
-        }
-        //OTHER ARM BINDS
-        if (armController.wasJustPressed(GamepadKeys.Button.Y) && armController.getButton(GamepadKeys.Button.START)) {
-            robotBase.airplaneLauncherSubsystem.raiseAndLaunch();
         }
             robotBase.mecanumDriveSubsystem.update();
 
