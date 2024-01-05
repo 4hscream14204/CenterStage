1cc64cb4ad92f59b892facee29471311d81fb739
OwenBaller <owenball@4hscream.org>
2024-01-04 20:52:15 -0600

    Worked on spike paths for blue right to backdrop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueRight.java
@@ -18,6 +18,9 @@ public class BlueRight extends OpMode {
     public RobotBase robotBase;
     private TrajectorySequence BlueRightPark;
     public Pose2d startPose;
+    public TrajectorySequence leftSpike;
+    public TrajectorySequence middleSpike;
+    public TrajectorySequence rightSpike;
 
     @Override
     public void init(){
@@ -31,6 +34,30 @@ public class BlueRight extends OpMode {
                 .lineTo(new Vector2d(6.00, 12.00))
                 .lineTo(new Vector2d(50.00, 12.00))
                 .build();
+
+        leftSpike = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(-38.35, 63.3, Math.toRadians(270.00)))
+                .splineToLinearHeading(new Pose2d(-36.00, 42.00, Math.toRadians(315.00)), Math.toRadians(315.00))
+                .lineToLinearHeading(new Pose2d(-36.00, 41.00, Math.toRadians(270.00)))
+                .splineTo(new Vector2d(-52, 600), Math.toRadians(0))
+                .lineTo(new Vector2d(25, 59))
+                .splineToConstantHeading(new Vector2d(48, 35), Math.toRadians(0))
+                        .build();
+
+        middleSpike = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(-38.35, 63.3, Math.toRadians(270.00)))
+                .splineToLinearHeading(new Pose2d(-36.00, 41.00, Math.toRadians(270.00)), Math.toRadians(315.00))
+                .lineToLinearHeading(new Pose2d(-36.00, 41.00, Math.toRadians(270.00)))
+                .splineTo(new Vector2d(-52, 600), Math.toRadians(0))
+                .lineTo(new Vector2d(25, 59))
+                .splineToConstantHeading(new Vector2d(48, 35), Math.toRadians(0))
+                        .build();
+
+        rightSpike = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(-38.35, 63.3, Math.toRadians(270.00)))
+                .splineToLinearHeading(new Pose2d(-37.00, 35.00, Math.toRadians(220.00)), Math.toRadians(261.00))
+                .lineToLinearHeading(new Pose2d(-36.00, 41.00, Math.toRadians(270.00)))
+                .splineTo(new Vector2d(-52, 600), Math.toRadians(0))
+                .lineTo(new Vector2d(25, 59))
+                .splineToConstantHeading(new Vector2d(48, 35), Math.toRadians(0))
+                        .build();
 
         robotBase.mecanumDriveSubsystem.setPoseEstimate(startPose);
     }
