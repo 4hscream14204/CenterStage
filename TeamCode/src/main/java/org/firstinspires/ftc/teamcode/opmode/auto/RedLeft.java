2e56e89f5af172c0284e2decb776457f8dfef774
PWikoff <prestonwikoff@4hscream.org>
2024-01-11 21:17:00 -0600

    Flipped auto blue routes to red side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/RedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/RedLeft.java
@@ -39,43 +39,29 @@ public class RedLeft extends OpMode {
         telemetry.update();
         startPose = new Pose2d(-38.35, -63.3, Math.toRadians(90.00));
         LeftSpike  = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(-38.35, -63.30, Math.toRadians(90.00)))
-                .splineTo(new Vector2d(-37.43, -43.15), Math.toRadians(103.54))
-                .waitSeconds(1)
-                .lineToConstantHeading(new Vector2d(-28.73, -58.72))
-                .lineToConstantHeading(new Vector2d(23.01, -59.87))
-                .splineToConstantHeading(new Vector2d(47.96, -29.80), Math.toRadians(0.00))
-                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(-47.00, -38.00, Math.toRadians(90.00)), Math.toRadians(90.00))
+                .lineToConstantHeading(new Vector2d(-33.00, -55.00))
+                .lineToLinearHeading(new Pose2d(-33.00, -21.00, Math.toRadians(90.00)))
+                .lineToLinearHeading(new Pose2d(-33.00, -12.00, Math.toRadians(180.00)))
+                .lineToLinearHeading(new Pose2d(25.00, -12.00, Math.toRadians(180.00)))
+                .splineToLinearHeading(new Pose2d(45.00, -27, Math.toRadians(180.00)), Math.toRadians(180.00))
                 .build();
-    /*.splineTo(new Vector2d(-43.00, -35.00), Math.toRadians(135.00))
-                .lineToSplineHeading(new Pose2d(-36.0, -59.00, Math.toRadians(180.00)))
-                .lineToLinearHeading(new Pose2d(54.00, -58.00, Math.toRadians(180)))
-                .build();*/
 
         MiddleSpike = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(-38.35, -63.30, Math.toRadians(90.00)))
-                .splineTo(new Vector2d(-36.52, -41.09), Math.toRadians(90.00))
-                .waitSeconds(1)
-                .splineToConstantHeading(new Vector2d(-28.73, -58.95), Math.toRadians(0.00))
-                .lineToConstantHeading(new Vector2d(23.00, -60.00))
-                .splineTo(new Vector2d(48.19, -36.52), Math.toRadians(0.00))
-                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(-36.00, -34.00, Math.toRadians(90.00)), Math.toRadians(90.00))
+                .lineToLinearHeading(new Pose2d(-34, -43, Math.toRadians(45.00)))
+                .splineToSplineHeading(new Pose2d(-5.00, -38.00, Math.toRadians(180.00)), Math.toRadians(180.00))
+                .splineToSplineHeading(new Pose2d(45.00, -36.00, Math.toRadians(180.00)), Math.toRadians(180.00))
                 .build();
-                /*.splineToLinearHeading(new Pose2d(-36.00, -34.00, Math.toRadians(90.61)), Math.toRadians(90.61))
-                .lineToSplineHeading(new Pose2d(-35.50, -60.00, Math.toRadians(180.00)))
-                .lineToLinearHeading(new Pose2d(54.00, -58.00, Math.toRadians(180)))
-                .build();*/
 
         RightSpike  = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(-38.35, -63.30, Math.toRadians(90.00)))
-                .splineTo(new Vector2d(-35.60, -38.58), Math.toRadians(90.00))
-                .waitSeconds(1)
-                .splineToConstantHeading(new Vector2d(-28.73, -58.95), Math.toRadians(0.00))
-                .lineToConstantHeading(new Vector2d(23.00, -60.00))
-                .splineTo(new Vector2d(48.42, -42.93), Math.toRadians(0.00))
-                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(-28.00, -39.00, Math.toRadians(45.00)), Math.toRadians(45.00))
+                .lineToSplineHeading(new Pose2d(-40.00, -50.00, Math.toRadians(90.00)))
+                .splineToLinearHeading(new Pose2d(-20.00, -12.00, Math.toRadians(180.00)), Math.toRadians(180.00))
+                //.waitSeconds(10)
+                .splineTo(new Vector2d(36.04, -24.19), Math.toRadians(315.))
+                .splineToSplineHeading(new Pose2d(45.00, -43.00, Math.toRadians(180.00)), Math.toRadians(270.00))
                 .build();
-                /*.splineToLinearHeading(new Pose2d(-30.00, -36.00, Math.toRadians(45.00)), Math.toRadians(45.00))
-                .lineToSplineHeading(new Pose2d(-40.00, -59.00, Math.toRadians(180.00)))
-                .lineToLinearHeading(new Pose2d(55.00, -58.00, Math.toRadians(180)))
-                .build();*/
 
 
         OuterPark = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(45.00, -36.00, Math.toRadians(180)))
