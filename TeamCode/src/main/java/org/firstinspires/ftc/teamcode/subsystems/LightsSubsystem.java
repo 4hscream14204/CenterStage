0521d8eabe41c40a70e0391c9af3b61dd1665287
FeignedBlock485 <benelven@4hscream.org>
2024-02-08 20:33:31 -0600

    Got the lights working with the chassis controller buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LightsSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LightsSubsystem.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class LightsSubsystem {
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.LED;
+
+public class LightsSubsystem extends SubsystemBase {
+
+    LED dgRedLight;
+    LED dgGreenLight;
+
+    public LightsSubsystem(LED redLightConstructor, LED greenLightConstructor){
+        dgRedLight = redLightConstructor;
+        dgGreenLight = greenLightConstructor;
+        lightOff();
+    }
+
+    public void lightOn (){
+        dgRedLight.enableLight(true);
+        dgGreenLight.enableLight(true);
+    }
+
+    public void lightOff (){
+        dgRedLight.enableLight(false);
+        dgGreenLight.enableLight(false);
+    }
 }
