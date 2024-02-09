baec9bb01b51d959a407d2d70b22e6fb52ffeaec
FeignedBlock485 <benelven@4hscream.org>
2024-02-08 21:07:12 -0600

    Worked on getting the TouchSensorSubsystem working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TouchSensorSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TouchSensorSubsystem.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class TouchSensorSubsystem {
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+public class TouchSensorSubsystem extends SubsystemBase {
+
+    DigitalChannel tsIntake;
+
+    public TouchSensorSubsystem (DigitalChannel intakeSensorConstructor) {
+        tsIntake = intakeSensorConstructor;
+    }
+
+    public boolean pixelInIntake(){
+        boolean bolPixelInIntake = false;
+        if(tsIntake.getState() == true){
+            bolPixelInIntake = true;
+        } else {
+            bolPixelInIntake = false;
+        }
+        return bolPixelInIntake;
+    }
 }
