807c00869059882589d13a70c4e2b3a18d7dfdab
PWikoff <prestonwikoff@4hscream.org>
2024-02-17 10:25:02 -0600

    Made half speed demo bot code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/DemoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/DemoBot.java
@@ -150,10 +150,10 @@ public class DemoBot extends LinearOpMode {
             */
 
             // Send calculated power to wheels
-            leftFrontDrive.setPower(leftFrontPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightBackDrive.setPower(rightBackPower);
+            leftFrontDrive.setPower(leftFrontPower/2);
+            rightFrontDrive.setPower(rightFrontPower/2);
+            leftBackDrive.setPower(leftBackPower/2);
+            rightBackDrive.setPower(rightBackPower/2);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
