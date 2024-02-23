9ef699b3afe027ab0775e21fa85c7ce0b8987abe
PWikoff <prestonwikoff@4hscream.org>
2024-02-22 19:52:17 -0600

    Worked on getting drop off to work for blue right left spike

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DropOffPositionLowCommandGrp.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitUntilCommand;
+
+import org.firstinspires.ftc.teamcode.hardware.RobotBase;
+import org.firstinspires.ftc.teamcode.subsystems.ArmSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.SlideSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.WristSubsystem;
+
+public class DropOffPositionLowCommandGrp extends SequentialCommandGroup {
+
+
+    public DropOffPositionLowCommandGrp(SlideSubsystem slideSubsystemCon,
+                                        ArmSubsystem armSubsystemCon,
+                                        WristSubsystem wristSubsystemCon,
+                                        IntakeSubsystem intakeSubsystemCon,
+                                        RobotBase.SlideHeight slideHeightCon) {
+        addCommands(
+                new InstantCommand(()->intakeSubsystemCon.intake(-0.5)),
+                new InstantCommand(()->armSubsystemCon.armDropOffLowestPos()),
+                new WaitUntilCommand(()->armSubsystemCon.armIsPassedWristSafe()),
+                new InstantCommand(()->wristSubsystemCon.wristDropOff()),
+                new InstantCommand(()->intakeSubsystemCon.intakeStop()),
+                new WaitUntilCommand(()->armSubsystemCon.armIsPassedExtendSlideSafe()),
+                new InstantCommand(()->slideSubsystemCon.slideGoToPos(slideHeightCon))
+        );
+    }
+}
