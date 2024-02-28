7018f7a5cb3905eaffc7392795de4f55b44e5ed4
OwenRex <owenrexshell@4hscream.org>
2024-02-27 18:13:40 -0600

    Made new logitech camera subsystem with better case logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/LogitechDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/LogitechDetectionTest.java
@@ -1,12 +1,63 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
-/*import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.hardware.RobotBase;
-import org.firstinspires.ftc.teamcode.subsystems.LogitechCameraSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.LogitechCameraSubsystemBetter;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.teamcode.subsystems.LogitechCameraSubsystemBetter;
+import android.util.Size;
 
-@Autonomous
-public class LogitechDetectionTest {
+@TeleOp(name="LogitechDetect")
+public class LogitechDetectionTest extends OpMode {
 
-public RobotBase robotBase;
-}*/
+    public RobotBase robotBase;
+    private LogitechCameraSubsystemBetter visionProcesser;
+    private VisionPortal visionPortal;
+
+    @Override
+    public void init() {
+      //  robotBase = new RobotBase(hardwareMap);
+
+        visionProcesser = new LogitechCameraSubsystemBetter(RobotBase.StartPosition.LEFT);
+      //  visionPortal = VisionPortal.easyCreateWithDefaults(
+               // hardwareMap.get(WebcamName.class, "Webcam1"),visionProcesser);
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam1"))
+                .addProcessor(visionProcesser)
+                .setCameraResolution(new Size(864, 480))
+                .enableLiveView(true)
+                .setAutoStopLiveView(true)
+                .build();
+
+
+/*
+        if (gamepad1.dpad_down) {
+            visionPortal.stopStreaming();
+        } else if (gamepad1.dpad_up) {
+            visionPortal.resumeStreaming();
+        }
+
+ */
+
+
+        //visionPortal = builder.build();
+
+    }
+
+    public void start (){
+        visionPortal.stopStreaming();
+    }
+
+@Override
+    public void loop(){
+
+    }
+
+
+
+}
+
