d8cb762b6172841d30a563a9d87bfcf8da53fe2f
FeignedBlock485 <benelven@4hscream.org>
2024-04-25 18:58:17 -0500

    Redesigned the RakeSubsystem as well as made a Rake operation for TeleDriverRobotControl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RakeSubsystem.java
@@ -6,18 +6,12 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class RakeSubsystem extends SubsystemBase {
 
     private Servo srvRake;
-    private double dblRakeDown = 1;
-    private double dblRakeUp = 0;
 
     public RakeSubsystem(Servo rakeConstructor){
         srvRake = rakeConstructor;
     }
 
-    public void rakeUp(){
-        srvRake.setPosition(dblRakeUp);
-    }
-
-    public void rakeDown(){
-        srvRake.setPosition(dblRakeDown);
+    public void rakePosition(double dblRakePosition){
+        srvRake.setPosition(dblRakePosition);
     }
 }
