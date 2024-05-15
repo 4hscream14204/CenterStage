f9caaeb99ae651b1b0f3edce82ca458335c679ab
OwenBaller <owenball@4hscream.org>
2024-05-14 20:49:06 -0500

    Made a class for duckhunt demo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleDuckHunt.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "DuckHunt")
+public class TeleDuckHunt {
+
+    public void init(){
+
+    }
+    public void loop(){
+
+    }
+}
