4775708d1dc0862d5aa042286fa561abb1954aa4
OwenBaller <owenball@4hscream.org>
2024-05-23 20:57:14 -0500

    Built the IntakeSubsystem and made the SlideSubsystem. also fleshed out RobotBase and started work on TeleDuckHunt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleDuckHunt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleDuckHunt.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import com.arcrobotics.ftclib.command.CommandScheduler;
@@ -18,7 +20,7 @@ import org.firstinspires.ftc.teamcode.subsystems.TimerSubsystem;
 import org.firstinspires.ftc.teamcode.hardware.RobotBase;
 
 @TeleOp(name = "DuckHunt")
-public class TeleDuckHunt {
+public class TeleDuckHunt extends OpMode {
 
     public RobotBase robotBase;
     private GamepadEx chassisController;
@@ -32,9 +34,24 @@ public class TeleDuckHunt {
 
     public void init(){
         CommandScheduler.getInstance().reset();
+        robotBase = new RobotBase(hardwareMap);
+        chassisController = new GamepadEx(gamepad1);
+        armController = new GamepadEx(gamepad1);
 
+        //CHASSIS CONTROLLER BINDS
+        //INTAKE OPERATION
+        new Trigger(()->chassisController.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.1)
+                .or(new Trigger(()-> chassisController.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.1))
+                .whileActiveContinuous(()-> CommandScheduler.getInstance().schedule(
+                        new InstantCommand(()-> robotBase.intakeSubsystem.intake(chassisController.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) -
+                                chassisController.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER))
+                        )
+                ))
+                .whenInactive(()-> CommandScheduler.getInstance().schedule(
+                        new InstantCommand(()-> robotBase.intakeSubsystem.intakeStop())
+                ));
     }
     public void loop(){
-
+    CommandScheduler.getInstance().run();
     }
 }
