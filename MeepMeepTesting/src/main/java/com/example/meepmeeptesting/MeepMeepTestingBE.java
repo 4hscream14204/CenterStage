ff679764b2162bc94a7fe9607048e94473ef7f9c
FeignedBlock485 <benelven@4hscream.org>
2024-07-11 13:20:12 -0500

    hopefully finished the route for the inner pickup on the blue side

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingBE.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingBE.java
@@ -12,28 +12,14 @@ public class MeepMeepTestingBE {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-41, -63.3, Math.toRadians(90.00)))
-                                .splineTo(new Vector2d(-40, -39.11), Math.toRadians(135.00))
+                        drive.trajectorySequenceBuilder(new Pose2d(45, 36, Math.toRadians(0)))
                                 .setReversed(true)
-                                .splineToSplineHeading(new Pose2d(-36.00, -60.00), Math.toRadians(360))
-                                //.addDisplacementMarker(() -> CommandScheduler.getInstance().schedule(new GrabAndWristEscapeCommandGrp(robotBase.leftWristSubsystem,
-                                //        robotBase.leftClawSubsystem,
-                                //        robotBase.armSubsystem)))
-                                .splineToConstantHeading(new Vector2d(12.00, -60.00), Math.toRadians(360))
-                                //.addDisplacementMarker(() -> CommandScheduler.getInstance().schedule(new DropOffPositionLowCommandGrp(robotBase.leftSlideSubsystem,
-                                //        robotBase.armSubsystem,
-                                //        robotBase.leftWristSubsystem,
-                                //        robotBase.intakeSubsystem,
-                                //        RobotBase.SlideHeight.LOWEST)))
-                                .splineToConstantHeading(new Vector2d(45.00, -26.00), Math.toRadians(360.00))
-                                .splineToConstantHeading(new Vector2d(51.00, -26.00), Math.toRadians(360.00))
-                                //.addDisplacementMarker(() -> CommandScheduler.getInstance().schedule(new ClawOpenCommand(robotBase.armSubsystem,
-                                //        robotBase.leftClawSubsystem)))
-                                //.addDisplacementMarker(() -> CommandScheduler.getInstance().schedule(new UniversalGrabbingPosCommand(robotBase)))
-                                .waitSeconds(0.5)
-                                .lineTo(new Vector2d(40.00, -36.00))
+                                .splineToConstantHeading(new Vector2d(25, 12),  Math.toRadians(180))
+                                .splineToConstantHeading(new Vector2d(-90, 12), Math.toRadians(180))
+                                .splineToConstantHeading(new Vector2d(25, 12), Math.toRadians(0.00))
+                                .splineToConstantHeading(new Vector2d(45, 36),  Math.toRadians(90))
                                 .build());
         //Don't delete lines below
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/StackPickupBlueInnerAutoCommandGrp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/StackPickupBlueInnerAutoCommandGrp.java
@@ -16,10 +16,11 @@ public class StackPickupBlueInnerAutoCommandGrp extends SequentialCommandGroup {
 
         cyclePose = poseConst;
         BlueStack = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(cyclePose)
-                .splineToConstantHeading(new Vector2d(25, 9),  Math.toRadians(270))
-                .splineToLinearHeading(new Pose2d(-90, 12, Math.toRadians(0.00)), Math.toRadians(180.00))
-                .splineToLinearHeading(new Pose2d(15, 12, Math.toRadians(0.00)), Math.toRadians(180.00))
-                .splineToConstantHeading(new Vector2d(45, 36),  Math.toRadians(270))
+                .setReversed(true)
+                .splineToConstantHeading(new Vector2d(25, 12),  Math.toRadians(180))
+                .splineToConstantHeading(new Vector2d(-90, 12), Math.toRadians(180))
+                .splineToConstantHeading(new Vector2d(25, 12), Math.toRadians(0.00))
+                .splineToConstantHeading(new Vector2d(45, 36),  Math.toRadians(90))
                 .build();
     }
 }
