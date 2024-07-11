9a05f2353353835b2326070267aa551375c76362
FeignedBlock485 <benelven@4hscream.org>
2024-07-11 15:26:14 -0500

    created StackPickupBlueOuterAutoCommandGrp and started moving Y coords to work for outer stack

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/StackPickupBlueOuterAutoCommandGrp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/StackPickupBlueOuterAutoCommandGrp.java
@@ -2,28 +2,26 @@ package org.firstinspires.ftc.teamcode.commands;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.arcrobotics.ftclib.command.CommandScheduler;
-import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.hardware.RobotBase;
 import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
 
-public class StackPickupBlueInnerAutoCommandGrp extends SequentialCommandGroup {
+public class StackPickupBlueOuterAutoCommandGrp extends SequentialCommandGroup {
     public RobotBase robotBase;
     public Pose2d cyclePose;
-    public StackPickupBlueInnerAutoCommandGrp(RobotBase robotBaseConst, Pose2d poseConst) {
+    public StackPickupBlueOuterAutoCommandGrp(RobotBase robotBaseConst, Pose2d poseConst) {
         robotBase = robotBaseConst;
         TrajectorySequence BlueStack;
 
         cyclePose = poseConst;
         BlueStack = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(cyclePose)
                 .setReversed(true)
-                .splineToConstantHeading(new Vector2d(25, 15),  Math.toRadians(180))
-                .splineToConstantHeading(new Vector2d(-108, 15), Math.toRadians(180))
+                .splineToConstantHeading(new Vector2d(25, 60),  Math.toRadians(180))
+                .splineToConstantHeading(new Vector2d(-108, 60), Math.toRadians(180))
                 .setReversed(false)
-                .splineToConstantHeading(new Vector2d(-98, 15), Math.toRadians(0.00))
-                .splineToConstantHeading(new Vector2d(25, 15), Math.toRadians(0.00))
+                .splineToConstantHeading(new Vector2d(-98, 60), Math.toRadians(0.00))
+                .splineToConstantHeading(new Vector2d(25, 60), Math.toRadians(0.00))
                 .splineToConstantHeading(new Vector2d(45, 36),  Math.toRadians(90))
                 .build();
     }
