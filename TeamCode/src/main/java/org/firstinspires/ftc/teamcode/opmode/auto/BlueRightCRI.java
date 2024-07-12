446d7b9a3ef9900319a0b704a8d70b2e8d746c19
PWikoff <prestonwikoff@4hscream.org>
2024-07-12 16:58:22 -0500

    Tuning routes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueRightCRI.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueRightCRI.java
@@ -79,7 +79,7 @@ public class BlueRightCRI extends OpMode {
                 .build();
         startPose = new Pose2d(-88.00, 61.00, Math.toRadians(0.00));
 
-        LeftSpike = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(-88.00, 61.00, Math.toRadians(270.00)))
+        LeftSpike = robotBase.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(-88.00, 61.00, Math.toRadians(0.00)))
                 .splineToLinearHeading(new Pose2d (-88,40, Math.toRadians(270.00)),Math.toRadians(270.00))
                 .splineToLinearHeading(new Pose2d( -74, 30, Math.toRadians(315.00)), Math.toRadians(315.00))
                 .splineToLinearHeading(new Pose2d(-96, 48, Math.toRadians(180.00)), Math.toRadians(180.00))
